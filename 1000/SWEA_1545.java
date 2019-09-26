4c1ae1bc6e5530697ec4726fa17a44762adbb43a
Se-young Hwang <nahwasa@gmail.com>
2019-09-26 17:03:29 +0900

    거꾸로 출력해 보아요
    이지

---
+++ 1000/SWEA_1545.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 		
+		int N = Integer.parseInt(br.readLine());
+		do {
+			bw.write(N + " ");
+		} while(N-->0);
+		bw.newLine();
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
