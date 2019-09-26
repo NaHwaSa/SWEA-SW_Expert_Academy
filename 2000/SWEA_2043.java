e2e9bc01f24035cff4b65aac25dfd9d247955c43
Se-young Hwang <nahwasa@gmail.com>
2019-09-26 14:44:42 +0900

    서랍의 비밀번호
    이지

---
+++ 2000/SWEA_2043.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		bw.write((Integer.parseInt(st.nextToken()) - Integer.parseInt(st.nextToken()) + 1) + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
